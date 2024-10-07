f0122c6a0feedf0b4086cbb9bd3a83a143cb7ba1
smeenakshi <meenakshi.s@inapp.com>
2024-10-07 16:19:52 +0530

    angular oct

--- udemy/src/app/app.module.ts
+++ udemy/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,HeaderComponent, UserComponent
   ],
   imports: [
     BrowserModule,
---
+++ udemy/src/app/user/user.component.ts
@@ -0,0 +1,18 @@
+import { Component} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+const randomIndex=Math.floor(Math.random() * DUMMY_USERS.length)
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+  selectedUser=DUMMY_USERS[randomIndex];
+  get imagePath(){
+    return 'assets/users/users/'+this.selectedUser.avatar
+  }
+  onSelectUser(){
+    console.log('Clicked');
+  }
+ 
+}
